812539515eaf80f2f65008f93f6ce6f08cf3fd96
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-07-25 03:26:51 +0530

    Initial Commit : Entity & DAO Created

---
+++ app/src/main/java/com/theleafapps/pro/roomwordssample/WordDao.java
@@ -0,0 +1,20 @@
+package com.theleafapps.pro.roomwordssample;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+
+    @Insert
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    List<Word> getAllWords();
+}
