b805028a6827848fac2579b7c23aec36833e7931
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-07-25 04:24:49 +0530

    Repository created for Word Class

---
+++ app/src/main/java/com/theleafapps/pro/roomwordssample/WordRepository.java
@@ -0,0 +1,42 @@
+package com.theleafapps.pro.roomwordssample;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class WordRepository {
+    private WordDao mWordDao;
+    private LiveData<List<Word>> mAllWords;
+
+    WordRepository(Application application){
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
+        mWordDao = db.wordDao();
+        mAllWords = mWordDao.getAllWords();
+    }
+
+    LiveData<List<Word>> getmAllWords(){
+        return mAllWords;
+    }
+
+    public void insert (Word word) {
+        new insertAsyncTask(mWordDao).execute(word);
+    }
+
+    private class insertAsyncTask extends AsyncTask<Word,Void,Void> {
+
+        private WordDao mAsyncTaskDao;
+
+        public insertAsyncTask(WordDao mWordDao) {
+            mAsyncTaskDao = mWordDao;
+        }
+
+        @Override
+        protected Void doInBackground(final Word... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+}
